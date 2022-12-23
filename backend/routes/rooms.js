5bf554e609d86638cc778acba379566d7ab6ca1a
Yash Raj <raj.yash1217@gmail.com>
2022-12-23 20:48:53 +0530

    Initial commit

---
+++ backend/routes/rooms.js
@@ -0,0 +1,20 @@
+const express=require("express")
+const router=express.Router()
+
+require("../utils/verifyToken")
+
+require("../controllers/rooms")
+
+router.post('/:hotelId',verifyAdmin,createRoom)
+
+
+//update
+router.put('/:id',verifyAdmin,updateRoom)
+//delete
+router.delete("/:id/:hotelId",verifyAdmin,deleteRoom)
+//get
+router.get('/:id',getRoom)
+//get all
+router.get('/',getAllRoom)
+
+module.exports=router
